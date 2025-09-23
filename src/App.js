8b2438deb86df5f952a8183623c0ae9677e71428
Gabriel-V-Martins <gabriel.martins318.dev@gmail.com>
2025-09-22 22:36:42 -0300

    Primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'; // Import de React
+import TodoList from "./TodoList"; // Import de TodoList (com TodoItem)
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+// Componente Principal
+export default function App() {
+
+  // Variável de estado que guarda todas atividades
+  const [todos, setTodos] = useState([]); // [] -> Lista, Array
+
+  // Variável de estado - Relativo à tarefa
+  const [newTask, setNewTask] = useState(""); // " " -> Tarefa, Texto
+
+  // Variável de estado - Relativo à Data
+  const [datadia, setDatadia] = useState(""); // " " -> Tarefa, Texto
+
+  // Variável de estado - Relativo à Hora
+  const [hora, setHora] = useState(""); // " " -> Tarefa, Texto
+
+  // Primeira Function:
+  // Adiciona Tarefa na Lista
+  const addTask = () => {
+    if (newTask.trim() === "") return // ".trim" -> "Raspa" os espaços vazios de uma String
+
+    // Criando um Objeto que represente a atividade
+    const newTodo = {
+      id: Date.now(), // Criação de ID único baseado na hora ATUAL (.now)
+      text: newTask,
+      date: datadia,
+      hora: hora
+    }
+
+    setTodos([...todos, newTodo]); // "..." -> Pega todos os elemento dentro do Array 'to-dos' e esparrama dentro do novo array junto de 'newTodo'
+
+    // Limpeza de variáveis de estado:
+    setNewTask("");
+    setDatadia("");
+    setHora("");
+  }
+
+  // Função de remoção de tarefas pelo id
+  const removeTask = (id) => (
+    setTodos(todos.filter((todo) => todo.id !== id)) // Filtra 'to-dos'
   );
-}
 
-export default App;
+  return (
+    <div>
+      <h1>Minha To-Do List</h1>
+      <div>
+        <input
+          type="text"
+          placeholder="Digite uma nova tarefa"
+          value={newTask}
+          onChange={(e) => setNewTask(e.target.value)} // "e" é referenciado ao Input/estímulo de evento
+        />
+        <button onClick={addTask}>Adicionar</button>
+      </div>
+
+      <div>
+        <TodoList todos={todos} removeTask={removeTask} />
+      </div>
+
+    </div>
+  )
+
+}
